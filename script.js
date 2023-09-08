02dab806a60d70fa85def9d0f835a1c1a6626381
rishi_raj <rishiraj007bgp@gmail.com>
2023-09-08 15:02:26 +0530

    apply grid 16*16

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const container = document.querySelector('.draw-pad');
 
+// function drawGrid(n) {
+//     for (let i = 0; i < n; i++) {
+//         const box = document.createElement('div');
+//         box.style.display = 'flex';
+        
+//     }
+// }
+
+
 for (let i = 0; i < 16; i++) {
-    const grid = document.createElement('div');
-    container.appendChild(grid)
-    
-    grid.style.width = '31px';
-    grid.style.height = '31px';
-    grid.style.border = '.25px solid grey';
+    const box = document.createElement('div');
+    box.style.display = 'flex';
+    box.style.flexDirection = "column";
+    container.appendChild(box);
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.style.height = '40px';
+        grid.style.width = '40px';
+        grid.style.border = '1px solid black';
+        box.appendChild(grid);
+    }
 }
-
-
-
-
-
