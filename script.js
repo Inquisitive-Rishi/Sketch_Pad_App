76e011b4285167830914d5416fa9cfa3daa70f36
rishi_raj <rishiraj007bgp@gmail.com>
2023-09-10 12:43:30 +0530

    add hover effect on grid

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const drawPad = document.querySelector('.draw-pad')
 
+const container = document.createElement('div');
+drawPad.appendChild(container);
+
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap';
+container.setAttribute('class', 'container');
 
 function drawGrid(n) {
     for (let i = 0; i < (n*n); i++) {
         const grid = document.createElement('div');
         grid.style.boxSizing = 'border-box';
-        grid.style.border = '.2px solid black';
+        grid.style.border = '1px solid black';
         grid.style.height = `${600/n}px`;
         grid.style.width = `${600/n}px`;
         grid.style.aspectRatio = 1;
-        drawPad.appendChild(grid);
+        grid.setAttribute('id', 'grid');
+        container.appendChild(grid);
     }
 }
+drawGrid(10);
 
-drawGrid(5);
+const grids = document.querySelectorAll('#grid');
+
+grids.forEach((grid)=> {
+    grid.addEventListener('mouseover', ()=> {
+        grid.classList.add('bgColor')
+    })
+    grid.addEventListener('mouseleave', ()=> {
+        grid.classList.remove('bgColor');
+    })
+})
